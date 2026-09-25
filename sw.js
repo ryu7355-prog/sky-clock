97b7b6066e185c66099062c33b0bd0667cd6c799
ryu7355-prog <ryu7355@gmail.com>
2026-09-25 19:50:01 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C = 'sky-clock-v2';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(C).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    if (e.request.method === 'GET' && res.ok) { const copy = res.clone(); caches.open(C).then(c => c.put(e.request, copy)); }
+    return res;
+  }).catch(() => r)));
+});
